7923c361a26944ae0aaa8fd73fd514ab77401d54
renharano <ri6bth6d@icloud.com>
2021-01-27 17:14:12 +0900

    商品情報編集機能訂正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:update, :show, :edit]
   before_action :contributor_confirmation, only: [:edit, :update]
   
   def index 
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    #@item = Item.find(params[:id])
   end
 
   def update
-    #@item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -44,7 +42,10 @@ class ItemsController < ApplicationController
   end
 
   def contributor_confirmation
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
