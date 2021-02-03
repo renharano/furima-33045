72cc85746393cc6c3008ea71f75eeda7798380e9
renharano <ri6bth6d@icloud.com>
2021-02-03 17:59:47 +0900

    商品購入訂正6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :basic_auth
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day])
-  #before_action :basic_auth
+  
   end
 
-  #private
-
-  #def basic_auth
-   # authenticate_or_request_with_http_basic do |username, password|
-    #  username == 'admin' && password == '1111'
-    #end
-  #end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '1111'
+    end
+  end
 end
